b9eea2ead1400ae85994f2e9f4db93b1a809ebfa
Kokonat007 <73847892+Kokonat007@users.noreply.github.com>
2024-05-10 16:14:17 +0000

    Update constants.ts
    Signed-off-by: Kokonat007 <73847892+Kokonat007@users.noreply.github.com>

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_PATH = import.meta.env.VITE_API_URL || "https://api.ree6.de"
+export const BASE_PATH = main.mc-vaxx.uk:25661
 export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://invite.ree6.de"
 
 export function get(path: string) {
